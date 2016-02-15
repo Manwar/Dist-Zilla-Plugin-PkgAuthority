f8a69fccb7dcc889dc5a93a052628d9291fd7a88
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-02-15 11:47:27 +0000

    - Added missing prereqs as reported by CPANTS.

--- lib/Dist/Zilla/Plugin/PkgAuthority.pm
+++ lib/Dist/Zilla/Plugin/PkgAuthority.pm
@@ -1,6 +1,6 @@
 package Dist::Zilla::Plugin::PkgAuthority;
 
-$Dist::Zilla::Plugin::PkgAuthority::VERSION   = '0.02';
+$Dist::Zilla::Plugin::PkgAuthority::VERSION   = '0.03';
 $Dist::Zilla::Plugin::PkgAuthority::AUTHORITY = 'cpan:MANWAR';
 
 use Moose;
@@ -144,7 +144,7 @@ Dist::Zilla::Plugin::PkgAuthority - Add a $AUTHORITY to your packages.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =head1 SYNOPSIS
 
